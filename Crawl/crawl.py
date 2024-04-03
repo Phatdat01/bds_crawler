a1edefa7a6baa9ff3c0bbd2a3e2aa0bb581cab72
Phatdat01 <phatdatlht2001@gmail.com>
2024-04-04 06:16:40 +0700

    dat: add code but error with sendkey

---
+++ Crawl/crawl.py
@@ -0,0 +1,8 @@
+from crawling import load_chrome, access_web
+
+URL = "https://batdongsan.vn/"
+def crawling():
+    driver = load_chrome()
+    access_web(need="Bán", province="Long An", url=URL, driver=driver)
+
+crawling();
---
+++ Crawl/crawling.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+def load_chrome() -> WebDriver:
+    service = Service()
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+def access_web(nees:str, province:str, url:str, driver:WebDriver):
+    driver.get(url=url)
+    wait = wait_element(driver=driver,timeout=120,key="css",by="select.need")
+    need = 
+
+
+def wait_element(driver: WebDriver, timeout: int, key:str, by:str) -> WebElement:
+    if by.lower() == "id":
+        condition = EC.visibility_of_element_located((By.ID, key))
+    elif by.lower() == "class":
+        condition = EC.visibility_of_element_located((By.CLASS_NAME, key))
+    elif by.lower() == "name":
+        condition = EC.visibility_of_element_located((By.NAME, key))
+    elif by.lower() == "tag":
+        condition = EC.visibility_of_element_located((By.TAG_NAME, key))
+    elif by.lower() == "css":
+        condition = EC.visibility_of_element_located((By.CSS_SELECTOR, key))
+    else:
+        return []
+
+    element = WebDriverWait(driver, timeout).until(condition)
+    return element
